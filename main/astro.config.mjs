8c815a83cc99a5b8803826632c25b06cbe72cf86
coppi3 <coppiemertz@gmail.com>
2022-11-21 22:44:01 +0300

    feat: 🎸 converted everything to astro

---
+++ main/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
+});
